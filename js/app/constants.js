be3975f33c6f3c9d6105db6fa405f30d356fdabe
Nils <nils@epita.fr>
2021-02-23 11:25:52 +0100

    update contant values

--- js/app/constants.js
+++ js/app/constants.js
@@ -14,13 +14,13 @@ var FieldNames = {
 
 var Constants = {
 	THRESHOLD_BUFFER_TIME_MS  :  (12 * 60 * 60 * 1000) ,
-	APP_SECRET  :  "bf908d1f-6a90-4fd3-8c17-a04840bba1d6", 
-	APP_ID  :  "5595bd7e-d5b7-46f4-a631-af39991d12c8",
-	PRIME_SERVER_ADDR  :  "https://captivateprime.adobe.com/", 
-	MY_APP_HOSTED_SERVER_BASE_ADDR  :  "http://localhost:8085/",
+	APP_SECRET  :  "",
+	APP_ID  :  "",
+	PRIME_SERVER_ADDR  :  "https://captivateprime.adobe.com/",
+	MY_APP_HOSTED_SERVER_BASE_ADDR  :  "http://localhost:8080/",
 	SCOPE  :  "learner:read,learner:write",
 	OCODE_DATA  :  "state1",
-	SOURCE_FOLDER  :  "PrimeApp/",
+	SOURCE_FOLDER  :  "",
 	MAIN_PAGE  :  "oauthredirect.html",
 	START_PAGE : "index.html",
 	API_VERSION: "primeapi/v2/",
